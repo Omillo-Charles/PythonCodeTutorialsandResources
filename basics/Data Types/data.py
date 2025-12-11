f5ffb5a9c3a53f6138ac4c044568e2ba8fc5eb64
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-12-12 01:41:10 +0300

    data types

---
+++ basics/Data Types/data.py
@@ -0,0 +1,19 @@
+#Text Type: str
+
+#Numeric Types: int, float, complex
+
+#Sequence Types: list, tuple, range
+
+#Mapping Type: dict
+
+#Set Types: set, frozenset
+
+#Boolean Type: bool
+
+#Binary Types: bytes, bytearray, memoryview
+
+#None Type: NoneType
+
+#Getting the data type
+name = "Omillo"
+print(type(name))
