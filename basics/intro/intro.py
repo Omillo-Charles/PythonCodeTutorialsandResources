114c200b3033145f369185d32c0922198c7b2b37
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-12-12 01:03:21 +0300

    introduction

---
+++ basics/intro/intro.py
@@ -0,0 +1,4 @@
+#If Python is not installed on your computer, you can download it for free from the official website: https://www.python.org/
+#Python is an interpreted programming language, this means that as a developer you write Python (.py) files in a text editor and then put those files into the python interpreter to be executed.
+greeting = "Hello World!"
+print(greeting) #outputs Hello World
