a3093af6b309dbe87abdb4ab695ae001e8ac7dfc
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-12-12 01:51:23 +0300

    numbers

---
+++ basics/numbers/numbers.py
@@ -0,0 +1,26 @@
+#INT
+#Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+a = 6
+b = 1234567
+c = -4567889
+print(type(a))
+print(type(b))
+print(type(c))
+
+#FLOAT
+#Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+d = 1.10
+e = 1.0
+f = -328.439
+print(type(d))
+print(type(e))
+print(type(f))
+
+#COMPLEX
+#Complex numbers are written with a "j" as the imaginary part
+g = 3+5j
+h = 5j
+i = -5j
+print(type(g))
+print(type(h))
+print(type(i))
