2a13b74c32e6ec0173da115cf4a8df67d4fb4af9
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-12-12 01:09:22 +0300

    syntax

---
+++ basics/syntax/syntax.py
@@ -0,0 +1,17 @@
+#Python syntax can be executed by writing directly in the Command Line
+#Or by creating a python file on the server, using the .py file extension, and running it in the Command Line
+#Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important. Python uses indentation to indicate a block of code.
+age = 18
+userAge = 23
+if userAge > age:
+    print("Allowed to vote!")
+
+#In Python, variables are created when you assign a value to it
+number = 12
+otherNumber = 23
+print(number)
+print(otherNumber)
+
+#Python has commenting capability for the purpose of in-code documentation.Comments start with a #, and Python will render the rest of the line as a comment:
+name = "Omillo"
+print(name) #outputs "Omillo"
