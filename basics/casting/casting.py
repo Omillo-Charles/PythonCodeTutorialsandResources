8fe3cd37bb6c9f487286f7719258528b6cefce85
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-12-12 01:54:33 +0300

    casting

---
+++ basics/casting/casting.py
@@ -0,0 +1,5 @@
+#There may be times when you want to specify a type on to a variable. This can be done with casting. Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+x = int(1)
+y = float(21)
+print(type(x))
+print(type(y))
