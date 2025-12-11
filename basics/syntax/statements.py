0614c44e3e9ea4b65c42dc474c8a3e09e082c2f0
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-12-12 01:12:33 +0300

    statements

---
+++ basics/syntax/statements.py
@@ -0,0 +1,10 @@
+#A computer program is a list of "instructions" to be "executed" by a computer. In a programming language, these programming instructions are called statements.
+name = "Omillo Fidel Charles"
+print(name) #This is a statement
+
+#You can add as many statements as you want
+print("Omillo Fidel Charles")
+print("I am a winner!")
+print("Impossible is nothing")
+
+#NOTE: Semicolons are optional
