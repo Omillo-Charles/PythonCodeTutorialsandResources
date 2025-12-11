ba8524db0c73fc21f268f719e3dee2a19f64ccb9
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-12-12 01:17:16 +0300

    output

---
+++ basics/output/numbers.py
@@ -0,0 +1,5 @@
+#You can also use the print() function to display numbers
+#However, unlike text, we don't put numbers inside double quotes
+print(123434)
+#You can also do math inside the print() function
+print(322*57676)
---
+++ basics/output/text.py
@@ -0,0 +1,8 @@
+#You can use the print() function as many times as you want to print text. Each call prints text on a new line by default
+#You can use double quotes or single quotes
+print("Hello World")
+#You can have as many statements as you wish
+print("Hello mate")
+print("Good morning")
+print("I am good")
+print("Hope you are well")
